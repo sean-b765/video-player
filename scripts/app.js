da34492cbc95ba59220ba97478a1e6bc39e64269
Sean Boaden <sean.b0153@gmail.com>
2021-05-17 14:49:33 +0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,141 @@
+const video = document.querySelector('video')
+
+const btnSizeMode = document.getElementById('size-mode')
+let fullscreen = false
+
+btnSizeMode.addEventListener('click', () => {
+	if (fullscreen) {
+		btnSizeMode.innerHTML = `<i class="fas fa-expand"></i>`
+		closeFullscreen()
+	} else {
+		btnSizeMode.innerHTML = `<i class="fas fa-compress"></i>`
+		openFullscreen()
+	}
+})
+video.addEventListener('dblclick', () => {
+	if (fullscreen) {
+		btnSizeMode.innerHTML = `<i class="fas fa-expand"></i>`
+		closeFullscreen()
+	} else {
+		btnSizeMode.innerHTML = `<i class="fas fa-compress"></i>`
+		openFullscreen()
+	}
+})
+
+document.onfullscreenchange = (e) => {
+	fullscreen = !fullscreen
+}
+
+function openFullscreen() {
+	if (document.querySelector('.video-container').requestFullscreen) {
+		document.querySelector('.video-container').requestFullscreen()
+	} else if (
+		document.querySelector('.video-container').webkitRequestFullscreen
+	) {
+		/* Safari */
+		document.querySelector('.video-container').webkitRequestFullscreen()
+	} else if (document.querySelector('.video-container').msRequestFullscreen) {
+		/* IE11 */
+		document.querySelector('.video-container').msRequestFullscreen()
+	}
+}
+
+function closeFullscreen() {
+	if (document.exitFullscreen) {
+		document.exitFullscreen()
+	} else if (document.webkitExitFullscreen) {
+		/* Safari */
+		document.webkitExitFullscreen()
+	} else if (document.msExitFullscreen) {
+		/* IE11 */
+		document.msExitFullscreen()
+	}
+}
+
+const btnPausePlay = document.getElementById('pause-play')
+btnPausePlay.addEventListener('click', () => {
+	if (video.paused || video.ended) {
+		video.play()
+	} else {
+		video.pause()
+	}
+})
+video.addEventListener('click', () => {
+	if (video.paused || video.ended) {
+		video.play()
+	} else {
+		video.pause()
+	}
+})
+
+const btnVolume = document.getElementById('volume')
+btnVolume.querySelector('.icon').addEventListener('click', () => {
+	if (video.muted) {
+		video.muted = false
+	} else {
+		video.muted = true
+	}
+})
+btnVolume.addEventListener('mouseenter', () => {
+	btnVolume.querySelector('.volume-popout').classList.add('shown')
+})
+btnVolume.addEventListener('mouseleave', () => {
+	btnVolume.querySelector('.volume-popout').classList.remove('shown')
+})
+
+video.onplay = () => {
+	btnPausePlay.innerHTML = `<i class="fas fa-pause"></i>`
+}
+video.onpause = () => {
+	btnPausePlay.innerHTML = `<i class="fas fa-play"></i>`
+}
+video.onended = () => {
+	console.log('ended')
+}
+
+video.onvolumechange = (e) => {
+	if (video.muted) {
+		btnVolume.querySelector(
+			'.icon'
+		).innerHTML = `<i class="fas fa-volume-mute"></i>`
+	} else {
+		btnVolume.querySelector(
+			'.icon'
+		).innerHTML = `<i class="fas fa-volume-down"></i>`
+	}
+}
+
+// When the video is playing, start the playback loop
+video.addEventListener('play', playLoop)
+function playLoop() {
+	video.playbackRate = 5
+	if (video.ended || video.paused) return
+
+	// Set the timestamp
+	const progressTime = timeIntToString(Math.floor(video.currentTime))
+	const fullTime = timeIntToString(Math.floor(video.duration))
+	document.getElementById(
+		'timestamp'
+	).innerHTML = `${progressTime} / ${fullTime}`
+
+	// Set the progress bar via percentage
+	const percentageComplete = (video.currentTime / video.duration) * 100
+	document.getElementById('progress').style.width = `${percentageComplete}%`
+
+	requestAnimationFrame(playLoop)
+}
+
+/*
+    Helper functions
+*/
+function timeIntToString(sec) {
+	let hours = Math.max(0, Math.floor(sec / 3600))
+	let mins = Math.max(0, Math.floor(sec / 60))
+	let secs = sec % 60
+
+	hours = hours === 0 ? (hours = '') : `${hours}:`
+	mins = mins === 0 ? (mins = '0:') : `${mins}:`
+	secs = secs === 0 ? (secs = '00') : `${secs < 10 ? `0${secs}` : secs}`
+
+	return `${hours}${mins}${secs}`.trim()
+}
